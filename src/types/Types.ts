03a38145eb5b42cdb2da331d19df629c706e23cf
freelinex <freelinex7@gmail.com>
2026-05-08 19:21:06 +0300

    add task soluiton

---
+++ src/types/Types.ts
@@ -0,0 +1,45 @@
+import type { Dispatch, SetStateAction } from 'react';
+
+export interface Todo {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+}
+
+export enum ErrorMessages {
+  empty = '',
+  notBeEmpty = 'Title should not be empty',
+  addError = 'Unable to add a todo',
+  deleteError = 'Unable to delete a todo',
+  loadError = 'Unable to load todos',
+  updateError = `Unable to update a todo`,
+}
+
+export type TodoListProps = {
+  filteredTodos: Todo[];
+  deletingTodoIds: number[];
+  checkedTodoId: number[];
+  tempTodo: Todo | null;
+  editingTodoId: number | null;
+  editedTitle: string;
+  handleDelete: (TodoId: number) => void;
+  handleMakeChecked: (updatedTodo: Todo) => void;
+  handleStartEditing: (todo: Todo) => void;
+  handleEditedTitleChange: (value: string) => void;
+  handleCancelEditing: () => void;
+  handleSubmitEditing: (todo: Todo) => void;
+};
+
+export type FooterProps = {
+  todos: Todo[];
+  filter: string;
+  setFilter: (filter: string) => void;
+  checkComplete: boolean;
+  handleClearCompleted: () => void;
+};
+
+export type ErrorNotificationsProps = {
+  errorMessage: ErrorMessages;
+  setErrorMessage: Dispatch<SetStateAction<ErrorMessages>>;
+};
